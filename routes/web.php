15e862e001eb9583f2681e1bc001e2447ee05974
JUAN PABLO GOMEZ GARCIA <Andrea-Ortiz>
2020-05-20 17:33:42 -0500

    Cree el panel del cliente

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::view('/','inicio')->name('inicio'); 
 Route::view('/contacto','inicio')->name('contacto'); 
 
+Route::view('/crearmascota','crearmascota')->name('crearmascota');
+Route::view('/PanelCliente','PanelC')->name('PanelC');
+Route::view('/actualizar','actualizar')->name('actualizar');
