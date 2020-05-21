a1dea6fe5bdb593fbd7906f1ca79598ffbfcdb48
JUAN PABLO GOMEZ GARCIA <Andrea-Ortiz>
2020-05-20 19:41:53 -0500

    adicione codigo a las vistas actualizar y crear mascotas

--- resources/views/actualizar.blade.php
+++ resources/views/actualizar.blade.php
@@ -6,7 +6,22 @@
 
 <h1 class="">Actualizar Perfil</h1>
 <hr>
-
+<div class="card-deck">
+    <div class="card mb-4" style="max-width: 540px;">
+        <div class="row no-gutters">
+            
+            <div class="col-md-8">
+                <div class="card-body">
+                    <p class="card-text">Nombre completo</p>
+                    <p class="card-text">Direccion</p>
+                    <p class="card-text">Telefono</p>
+                    <p class="card-text">Nombre mascota</p>
+                    <a href="#" class="btn btn-primary">Guardar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
 
