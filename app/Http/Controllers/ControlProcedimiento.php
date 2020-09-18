224a82e73a0e4ef85bbf0598fae23f52b3529de9
Viviana Ortiz <andre.vivi.92@gmail.com>
2020-09-17 19:49:59 -0500

    inclusion de llaves foraneas en prodedimiento

--- app/Http/Controllers/ControlProcedimiento.php
+++ app/Http/Controllers/ControlProcedimiento.php
@@ -20,81 +20,56 @@ class ControlProcedimiento extends Controller
         return view('procedimientos.indexProcedi',compact('procedimiento'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('procedimientos.crearProcedi');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request, Procedimiento $index)
     {
         $procedi=[];
-        $procedi['idProc']=$request->get('idProc');
-        $procedi['fecha']=$request->get('fecha');
+        $procedi['fecha']= str_replace('-','/',$request->get('fecha'));
         $procedi['sigDosis']=$request->get('sigDosis');
+        $procedi['mascota_idMascotas'] = $request->get('idMascotas');
+        $procedi['vacunas_idVacun'] = $request->get('idVacun');
+        $procedi['despara_idDespara'] = $request->get('idDespara');
+        $procedi['veterin_idVeterin'] = $request->get('idVeterin');
+        $procedi['examenes_idExam'] = $request->get('idExam');
+        $procedi['visible'] = 1;
         $index->guardarprocedi($procedi);
 
         return redirect()->route('indexprocedi')->with('estado', 'Se ha creado con exito');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $procedi=$this->modelo->mostrarProcedi($id);
-        return view('procedimientos.showProcedi',compact('procedi'));
+        return view('procedimientos.showProcedi', compact('procedi'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $procedi=$this->modelo->mostrarProcedi($id);
         return view('procedimientos.editProcedi',compact('procedi','id'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $procedi=[];
-        $procedi['idProc']=$request->get('idProc');
-        $procedi['fecha']=$request->get('fecha');
+        $procedi['fecha']= str_replace('-','/',$request->get('fecha'));
         $procedi['sigDosis']=$request->get('sigDosis');
+        $procedi['mascota_idMascotas'] = $request->get('idMascotas');
+        $procedi['vacunas_idVacun'] = $request->get('idVacun');
+        $procedi['despara_idDespara'] = $request->get('idDespara');
+        $procedi['veterin_idVeterin'] = $request->get('idVeterin');
+        $procedi['examenes_idExam'] = $request->get('idExam');
+        $procedi['idProc'] = $id;
+        $procedi['visible'] = 1;
         
         $this->modelo->Actualizar($procedi);
         return redirect()->route('indexprocedi')->with('estado', 'Se ha actualizado con exito');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $procedi['idProc']=$id;
