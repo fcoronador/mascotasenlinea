f16951442fc8c81b29564a34ed7a39f0912ed3cb
Fredy Coronado <fecoronado9@misena.edu.co>
2020-10-13 19:36:51 -0500

    corrige el bug de cliente

--- app/Http/Controllers/ControlCliente.php
+++ app/Http/Controllers/ControlCliente.php
@@ -15,14 +15,16 @@ class ControlCliente extends Controller
         $this->modelo = new Cliente();
     }
 
-  
-    public static function listClientes(){
-        $modelo= new Cliente();
+
+    public static function listClientes()
+    {
+        $modelo = new Cliente();
         $clientes = $modelo->indexclientes()->getClientes();
         return $clientes;
     }
-    public static function cantClientes(){
-        $modelo= new Cliente();
+    public static function cantClientes()
+    {
+        $modelo = new Cliente();
         $cantidad = $modelo->adminCliente();
         return $cantidad;
     }
@@ -61,32 +63,32 @@ class ControlCliente extends Controller
      */
     public function store(Request $request, Cliente $index)
     {
-        
+
         request()->validate([
-        'idCedula'=>'required|digits_between:5,20',
-        'nombre'=>'required|alpha_dash|between:3,39',
-        'apellido'=>'required|alpha_dash|between:3,39',
-        'telefono'=>'required|digits_between:7,14',
-        'direccion'=>'required|alpha_dash|alpha_num|max:74',
-        'correo'=>'required|email',
-        'contrasena'=>'required'
-        ],[
-            'idCedula.required'=>'Se necesita el número de identificación.',
-            'idCedula.digits_between'=>'La longitud del número de identificación debe estar entre 5-20 caracteres.',
-            'nombre.required'=>'Se necesita el nombre del cliente.',
-            'nombre.between'=>'La longitud del nombre debe estar entre 3-39 caracteres.',
-            'apellido.required'=>'Se necesita el nombre del cliente.',
-            'apellido.between'=>'La longitud del nombre debe estar entre 3-39 caracteres.',
-            'telefono.required'=>'Se necesita el número de teléfono.',
-            'telefono.digits_between'=>'La longitud del número de teléfono debe estar 7-14 dígitos.',
-            'direccion.required'=>'Se necesita la dirección del cliente',
-            'direccion.max'=>'La longitud máxima debe ser de 74 dígitos',
-            'correo.required'=>'Se necesita el correo del cliente.',
-            'correo.email'=>'Se  necesita un email valido.',
-            'contrasena.required'=>'Se necesita la contraseña'
+            'idCedula' => 'required',
+            'nombre' => 'required|between:3,39',
+            'apellido' => 'required|between:3,39',
+            'telefono' => 'required',
+            'direccion' => 'required|max:74',
+            'correo' => 'required',
+            'contrasena' => 'required'
+        ], [
+            'idCedula.required' => 'Se necesita el número de identificación.',
+            'idCedula.digits_between' => 'La longitud del número de identificación debe estar entre 5-20 caracteres.',
+            'nombre.required' => 'Se necesita el nombre del cliente.',
+            'nombre.between' => 'La longitud del nombre debe estar entre 3-39 caracteres.',
+            'apellido.required' => 'Se necesita el nombre del cliente.',
+            'apellido.between' => 'La longitud del nombre debe estar entre 3-39 caracteres.',
+            'telefono.required' => 'Se necesita el número de teléfono.',
+            'telefono.digits_between' => 'La longitud del número de teléfono debe estar 7-14 dígitos.',
+            'direccion.required' => 'Se necesita la dirección del cliente',
+            'direccion.max' => 'La longitud máxima debe ser de 74 dígitos',
+            'correo.required' => 'Se necesita el correo del cliente.',
+            'correo.email' => 'Se  necesita un email valido.',
+            'contrasena.required' => 'Se necesita la contraseña'
         ]);
-        
-        
+
+
         $cliente = [];
         $cliente['idCedula'] = $request->get('idCedula');
         $cliente['nombre'] = $request->get('nombre');
@@ -110,15 +112,13 @@ class ControlCliente extends Controller
     public function show($id)
     {
         $cliente = $this->modelo->mostrarCliente($id);
-       
 
-        if(session('rol')=== 3 )
-        {
+
+        if (session('rol') === 3) {
             return view('cliente.perfil', compact('cliente'));
-        }else if(session('rol')===1 || session('rol')===2){
+        } else if (session('rol') === 1 || session('rol') === 2) {
             return view('admin.showCliente', compact('cliente'));
         }
-
     }
 
     /**
@@ -151,29 +151,29 @@ class ControlCliente extends Controller
     {
 
         request()->validate([
-            'idCedula'=>'required|digits_between:5,20',
-            'nombre'=>'required|alpha_dash|between:3,39',
-            'apellido'=>'required|alpha_dash|between:3,39',
-            'telefono'=>'required|digits_between:7,14',
-            'direccion'=>'required|alpha_num|max:74',
-            'correo'=>'required|email',
-            'contrasena'=>'required'
-            ],[
-                'idCedula.required'=>'Se necesita el número de identificación.',
-                'idCedula.digits_between'=>'La longitud del número de identificación debe estar entre 5-20 caracteres.',
-                'nombre.required'=>'Se necesita el nombre del cliente.',
-                'nombre.between'=>'La longitud del nombre debe estar entre 3-39 caracteres.',
-                'apellido.required'=>'Se necesita el nombre del cliente.',
-                'apellido.between'=>'La longitud del nombre debe estar entre 3-39 caracteres.',
-                'telefono.required'=>'Se necesita el número de teléfono.',
-                'telefono.digits_between'=>'La longitud del número de teléfono debe estar 7-14 dígitos.',
-                'direccion.required'=>'Se necesita la dirección del cliente',
-                'direccion.max'=>'La longitud máxima debe ser de 74 dígitos',
-                'correo.required'=>'Se necesita el correo del cliente.',
-                'correo.email'=>'Se  necesita un email valido.',
-                'contrasena.required'=>'Se necesita la contraseña'
-            ]);
-            
+            'idCedula' => 'required|digits_between:5,20',
+            'nombre' => 'required|alpha_dash|between:3,39',
+            'apellido' => 'required|alpha_dash|between:3,39',
+            'telefono' => 'required|digits_between:7,14',
+            'direccion' => 'required|alpha_num|max:74',
+            'correo' => 'required|email',
+            'contrasena' => 'required'
+        ], [
+            'idCedula.required' => 'Se necesita el número de identificación.',
+            'idCedula.digits_between' => 'La longitud del número de identificación debe estar entre 5-20 caracteres.',
+            'nombre.required' => 'Se necesita el nombre del cliente.',
+            'nombre.between' => 'La longitud del nombre debe estar entre 3-39 caracteres.',
+            'apellido.required' => 'Se necesita el nombre del cliente.',
+            'apellido.between' => 'La longitud del nombre debe estar entre 3-39 caracteres.',
+            'telefono.required' => 'Se necesita el número de teléfono.',
+            'telefono.digits_between' => 'La longitud del número de teléfono debe estar 7-14 dígitos.',
+            'direccion.required' => 'Se necesita la dirección del cliente',
+            'direccion.max' => 'La longitud máxima debe ser de 74 dígitos',
+            'correo.required' => 'Se necesita el correo del cliente.',
+            'correo.email' => 'Se  necesita un email valido.',
+            'contrasena.required' => 'Se necesita la contraseña'
+        ]);
+
         $cliente = [];
         $cliente['idCedula'] = $request->get('idCedula');
         $cliente['nombre'] = $request->get('nombre');
@@ -184,10 +184,9 @@ class ControlCliente extends Controller
         $cliente['contrasena'] = $request->get('contrasena');
 
         $this->modelo->Actualizar($cliente);
-        if(session('rol')=== 3 )
-        {
+        if (session('rol') === 3) {
             return redirect()->route('usuario')->with('estado', 'El cliente se ha actualizado con éxito');
-        }else if(session('rol')===1 || session('rol')===2){
+        } else if (session('rol') === 1 || session('rol') === 2) {
             return redirect()->route('indexcliente')->with('estado', 'El cliente se ha actualizado con éxito');
         }
     }
@@ -200,8 +199,8 @@ class ControlCliente extends Controller
      */
     public function destroy($id)
     {
-        $cliente['idCedula']=$id;
-        $cliente['visible']= false;
+        $cliente['idCedula'] = $id;
+        $cliente['visible'] = false;
         $this->modelo->borrar($cliente);
         return redirect()->route('indexcliente')->with('estado', 'El cliente se ha sido eliminado con exito');
     }
